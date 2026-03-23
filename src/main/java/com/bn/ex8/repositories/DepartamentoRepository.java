09e254b5513dfd5af5622e156f6a2f4c0f0c1904
Matheus <178436024+maxmati1@users.noreply.github.com.>
2026-03-22 22:21:57 -0300

    Adiciona repository Departamento

---
+++ src/main/java/com/bn/ex8/repositories/DepartamentoRepository.java
@@ -0,0 +1,7 @@
+package com.bn.ex8.repositories;
+
+import com.bn.ex8.models.DepartamentoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<DepartamentoModel, Long> {
+}
